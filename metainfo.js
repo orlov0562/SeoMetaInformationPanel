9cfa8d9d17f3ba73ebc542ce35153fcf47184556
Vitaliy S. Orlov <orlov0562@gmail.com>
2018-07-05 20:49:42 +0300

    Update metainfo.js

--- metainfo.js
+++ metainfo.js
@@ -5,12 +5,16 @@
 // @description  Shows information from meta tags of the page
 // @author       Vitaliy Orlov
 // @match        *://*/*
+// @exclude      https://github.com/*
+// @exclude      https://*google.*/*
 // @grant GM_setValue
 // @grant GM_getValue
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    if (window.self !== window.top) return false;
 
     class Z {
         constructor(el){
@@ -55,6 +59,11 @@
             return this;
         }
 
+        prepend(innerHTML) {
+            this.el.innerHTML = innerHTML + this.el.innerHTML;
+            return this;
+        }
+
         html(innerHTML) {
             if (typeof innerHTML == 'undefined') {
                 return this.el.innerHTML;
@@ -83,6 +92,18 @@
             return this;
         }
 
+        static ready(func, timeout) {
+            var domReadyFunc = function(){
+                document.removeEventListener( "DOMContentLoaded", domReadyFunc, false );
+                if (typeof timeout != 'undefined') {
+                    setTimeout(func, timeout);
+                } else {
+                    func();
+                }
+            };
+            document.addEventListener("DOMContentLoaded", domReadyFunc, false );
+        }
+
         addClass(className){this.el.classList.add(className);return this;}
         removeClass(className){this.el.classList.remove(className);return this;}
         toggleClass(className){this.el.classList.toggle(className);return this;}
@@ -99,16 +120,17 @@
     if (!charset) charset = Z.find('meta[http-equiv]').attr("content");
     var viewport = Z.find('meta[name="viewport"]').attr("content");
     var robots = Z.find('meta[name="robots"]').attr("content");
+    if (robots) robots = robots.replace('noindex', '<span class="metainfo_redhl">noindex</span>').replace('nofollow', '<span class="metainfo_redhl">nofollow</span>');
+
     var css = '';
     css += '.metainfo_panel {position:fixed; bottom:0px; right:0px; background-color:#eee; border: 1px solid #444; max-width:25%; z-index:100000; display:flex;}';
-    css += '.metainfo_panel table th, .metainfo_panel table td {padding:0px 5px; font-size:12px; font-family:Verdana; border-bottom:1px solid #ccc;}';
+    css += '.metainfo_panel table th, .metainfo_panel table td {padding:0px 5px; font-size:12px; font-family:Verdana; border-bottom:1px solid #ccc; color:black;}';
     css += '.metainfo_panel table tr th {background-color:#e0e0e0; font-weight:normal; text-align:left;}';
     css += '.metainfo_panel table tr td.center {text-align:center;}';
     css += '.metainfo_panel table {margin:5px 5px 5px 5px;}';
     css += '.metainfo_hidden {display:none;}';
-    css += '#metainfo_toggle_btn {display:block; padding:5px; line-height:14px; text-align:center; text-decoration:none; background-color:#444;; color:white; font-size:12px; font-weight:bold;};';
-
-    Z.find('head').el.appendChild(Z.create('style').attr('type','text/css').append(css).el);
+    css += '#metainfo_toggle_btn {display:block; padding:5px; line-height:14px; text-align:center; text-decoration:none; background-color:#444;; color:white; font-size:12px; font-weight:bold;}';
+    css += '.metainfo_redhl {display:inline-block; background-color:red; color:white; padding: 0 2px;}';
 
     var miPanelVisible = GM_getValue("metainfo_panel_visible", false);
 
@@ -126,13 +148,15 @@
     html += '</table>';
     html += '</div>';
 
-    Z.find('body').append(html);
-
-    Z.find('#metainfo_toggle_btn').on('click', function(){
-        Z.find('#metainfo_table').toggleClass('metainfo_hidden');
-        GM_setValue("metainfo_panel_visible", !Z.find('#metainfo_table').hasClass('metainfo_hidden'));
-        this.blur();
-        return false;
+   Z.ready(function(){
+        Z.find('head').el.appendChild(Z.create('style').attr('type','text/css').append(css).el);
+        Z.find('body').prepend(html);
+        Z.find('#metainfo_toggle_btn').on('click', function(){
+            Z.find('#metainfo_table').toggleClass('metainfo_hidden');
+            GM_setValue("metainfo_panel_visible", !Z.find('#metainfo_table').hasClass('metainfo_hidden'));
+            this.blur();
+            return false;
+        });
     });
 
     // ==============================================
