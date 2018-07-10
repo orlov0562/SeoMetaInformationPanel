07b50a3ecc10e1dfdee83ef59e98509fa4a98974
Vitaliy S. Orlov <orlov0562@gmail.com>
2018-07-10 10:33:39 +0300

    Update metainfo.js

--- metainfo.js
+++ metainfo.js
@@ -116,14 +116,17 @@
     var description = Z.find('meta[name="description"]').attr("content");
     var keywords = Z.find('meta[name="keywords"]').attr("content");
     var canonical = Z.find('link[rel="canonical"]').attr("href");
+    var amphtml = Z.find('link[rel="amphtml"]').attr("href");
     var charset = Z.find('meta[charset]').attr("charset");
     if (!charset) charset = Z.find('meta[http-equiv]').attr("content");
     var viewport = Z.find('meta[name="viewport"]').attr("content");
     var robots = Z.find('meta[name="robots"]').attr("content");
     if (robots) robots = robots.replace('noindex', '<span class="metainfo_redhl">noindex</span>').replace('nofollow', '<span class="metainfo_redhl">nofollow</span>');
+    var googlebot = Z.find('meta[name="googlebot"]').attr("content");
+    if (googlebot) googlebot = googlebot.replace('noindex', '<span class="metainfo_redhl">noindex</span>').replace('nofollow', '<span class="metainfo_redhl">nofollow</span>');
 
     var css = '';
-    css += '.metainfo_panel {position:fixed; bottom:0px; right:0px; background-color:#eee; border: 1px solid #444; max-width:25%; z-index:100000; display:flex;}';
+    css += '.metainfo_panel {position:fixed; bottom:0px; right:0px; background-color:#eee; border: 1px solid #444; max-width:480px; z-index:100000; display:flex;}';
     css += '.metainfo_panel table th, .metainfo_panel table td {padding:0px 5px; font-size:12px; font-family:Verdana; border-bottom:1px solid #ccc; color:black;}';
     css += '.metainfo_panel table tr th {background-color:#e0e0e0; font-weight:normal; text-align:left;}';
     css += '.metainfo_panel table tr td.center {text-align:center;}';
@@ -141,23 +144,26 @@
     html += '<tr><th>Title</th><td>'+(title?title:'-')+'</td><td class="center">['+(title?title.length:'0')+']</td></tr>';
     html += '<tr><th>Description</th><td>'+(description?description:'-')+'</td><td class="center">['+(description?description.length:'0')+']</td></tr>';
     html += '<tr><th>Keywords</th><td>'+(keywords?keywords:'-')+'</td><td class="center">['+(keywords?keywords.split(',').length:'0')+']</td></tr>';
-    html += '<tr><th>Canonical</th><td colspan="2">'+(canonical?canonical:'-')+'</td></tr>';
+    html += '<tr><th>Canonical</th><td>'+(canonical?canonical:'-')+'</td><td class="center">['+(canonical?'<a href="'+canonical+'">go</a>':'-')+']</td></tr>';
+    html += '<tr><th>AMP &#9889;</th><td>'+ (amphtml ? amphtml : '-') + '</td><td class="center">['+ (amphtml ? '<a href="'+amphtml+'">go</a>' : '-')+']</td></tr>';
     html += '<tr><th>Charset</th><td colspan="2">'+(charset?charset:'-')+'</td></tr>';
     html += '<tr><th>Viewport</th><td colspan="2">'+(viewport?viewport:'-')+'</td></tr>';
     html += '<tr><th>Robots</th><td colspan="2">'+(robots?robots:'-')+'</td></tr>';
+    html += '<tr><th>GoogleBot</th><td colspan="2">'+(googlebot?googlebot:'-')+'</td></tr>';
     html += '</table>';
     html += '</div>';
 
-   Z.ready(function(){
-        Z.find('head').el.appendChild(Z.create('style').attr('type','text/css').append(css).el);
-        Z.find('body').prepend(html);
-        Z.find('#metainfo_toggle_btn').on('click', function(){
-            Z.find('#metainfo_table').toggleClass('metainfo_hidden');
-            GM_setValue("metainfo_panel_visible", !Z.find('#metainfo_table').hasClass('metainfo_hidden'));
-            this.blur();
-            return false;
-        });
-    }, 1000);
+
+    Z.ready(function(){
+       Z.find('head').el.appendChild(Z.create('style').attr('type','text/css').append(css).el);
+       Z.find('body').prepend(html);
+       Z.find('#metainfo_toggle_btn').on('click', function(){
+           Z.find('#metainfo_table').toggleClass('metainfo_hidden');
+           GM_setValue("metainfo_panel_visible", !Z.find('#metainfo_table').hasClass('metainfo_hidden'));
+           this.blur();
+           return false;
+       });
+    }, 500);
 
     // ==============================================
 })();
